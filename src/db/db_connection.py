23f1810a7655df6a780b089e92415d714b07e255
Rohit Karthick <rkbeo5@gmail.com>
2025-12-25 18:55:59 +0530

    chore(db): add database connection helper

---
+++ src/db/db_connection.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+# Load environment variables
+load_dotenv()
+
+def get_db_connection():
+
+    # Read DB config from environment
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME")
+
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
+    return conn
